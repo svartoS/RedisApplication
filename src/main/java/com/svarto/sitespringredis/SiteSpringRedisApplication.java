195b38fc622c8daf2aa4e9f9ec9391ae94bb21ec
svartoS <i.apevalin@gmail.com>
2024-07-04 00:20:16 +0400

    EZ

--- src/main/java/com/svarto/sitespringredis/SiteSpringRedisApplication.java
+++ src/main/java/com/svarto/sitespringredis/SiteSpringRedisApplication.java
@@ -10,7 +10,7 @@ public class SiteSpringRedisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiteSpringRedisApplication.class, args);
-		System.out.println("xd");
+		System.out.println("У МЕНЯ ПОХОДУ РАБОТАЕТ ЭТО ГОВНИЩЕ )))");
 	}
 
 }
